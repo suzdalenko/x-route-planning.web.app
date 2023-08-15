b817048295c3a231a25f02ca41c2c708175db576
suzdalenko.suzdalenko@gmail.com <suzdalenko.suzdalenko@gmail.com>
2023-08-15 14:51:26 +0200

    hjh

--- public/pages/assets/js/routeview.js
+++ public/pages/assets/js/routeview.js
@@ -10,7 +10,9 @@ var map, directionsService, directionsRenderer
 var listadoMarkers          = []
 var listdoRealMarkers       = []
 var kg_route                = 0
+var pl_route                = 0
 var leenda_orders           = ''
+var routeName               = ''
 
 function getRouteTrack(){
     listadoEntregas   = []
@@ -18,13 +20,14 @@ function getRouteTrack(){
     waypoints         = []
     listadoMarkers    = []
     kg_route          = 0
+    pl_route          = 0
     leenda_orders     = ""
 
     fetch(MYSITE_URL+'lines_collection/orders_by_route/?collection_id='+COLLECTION_ID+"&truck_id="+TRACK_ID).then(res => res.json()).then(res => { 
         let firstElement = res.result[0]
         lstadoLocations.push(new google.maps.LatLng(firstElement.plat, firstElement.plng))
         // spanRoute
-        let routeName = firstElement.truck+'. '
+        routeName = firstElement.truck+'. '
         firstElement.truck_name ? routeName += firstElement.truck_name : routeName += ' Camion'
         document.getElementById("spanRoute").innerHTML = routeName
 
@@ -43,6 +46,7 @@ function getRouteTrack(){
             listadoMarkers.push(ubicacion)
             waypoints.push({location: new google.maps.LatLng(item.clat, item.clng)})
             kg_route += item.kilos
+            pl_route += item.palets
             leenda_orders += `<div class="itemPedido" draggable="true" ondragstart="dragFunction(event,`+item.id+`,`+item.by_order+`)" ondrop="dropFunction(event,`+item.id+`,`+item.by_order+`)" ondragover="allowDropFunction(event)">
                                 `+countOrder+`. Pedido-`+item.order_id+`, `+item.client_name+`<br>
                                 Paletas `+item.palets+`, Kilos `+item.kilos+`<br>
@@ -98,24 +102,36 @@ function mostrarListadoPedidos(x){
    
 }
 
-function moderarRegionCuidad(){
-    return "x"
-}
-
 function computeTotalDistance(result) {
     let total = 0;
     const myroute = result.routes[0];
     if (!myroute) { return; }
-    for (let i = 0; i < myroute.legs.length; i++) {
-      total += myroute.legs[i].distance.value;
-    }
+    for (let i = 0; i < myroute.legs.length; i++) { total += myroute.legs[i].distance.value; }
     total = parseInt(total / 1000)
     document.getElementById("totalA").innerHTML = 'Distancia '+total+' km'
     document.getElementById("kgA").innerHTML = 'Kilos '+kg_route+' kg'
-    // document.title = ' Distancia Total ' +  total + ' km'
+    document.title = routeName+' '+total+' km'
+    document.getElementById("palA").innerHTML = 'Paletas '+pl_route
+}
+
+document.getElementById("recalcRoute").addEventListener("click", () => {
+  let recalcularRuta = confirm('¿Recalcular Ruta?')
+  if(recalcularRuta){
+    LoaderRoutePlanner('block')
+    let formData = new FormData()
+        formData.append("user_id", USER_ID)
+        formData.append("uid", UID)
+        formData.append("collection_id", COLLECTION_ID)
+        formData.append("truck_id", TRACK_ID)
+    fetch(MYSITE_URL+'post_parameters/recalculate_route/', {method: "POST", body: formData}).then(res => res.json()).then(res => {
+      initMap()
+      LoaderRoutePlanner('none')
+    }).catch(e => {
+      initMap()
+      LoaderRoutePlanner('none')
+    })  
   }
-
-
+})
 
 
 
